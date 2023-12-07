92c6358c80d4f1ab4602a40be00951e2646f7451
Alek Jarmov <alek.jarmov@gmail.com>
2023-12-08 00:13:27 +0100

    add d7

---
+++ 2023/d7/sol2.py
@@ -0,0 +1,101 @@
+from functools import cmp_to_key
+from collections import Counter
+
+
+def read_input():
+    with open('input') as f:
+        return f.read().splitlines()
+
+
+card_types = ['A', 'K', 'Q', 'T', '9', '8', '7', '6', '5', '4', '3', '2', 'J']
+
+
+def get_hand_ranking(hand):
+    hand = sorted(hand)
+    counts = Counter(hand)
+
+    counts = {k: v for k, v in counts.items() if k != 'J'}
+    cnt_vals = list(counts.values())
+    joker_count = hand.count('J')
+    if counts == {}:
+        return 7
+    maxi_count = max(counts.values())
+    total_cnt = joker_count + maxi_count
+    if joker_count == 0:
+        if 5 in counts.values():
+            return 7  # Five of a kind
+        elif 4 in counts.values():
+            return 6  # Four of a kind
+        elif 3 in counts.values() and 2 in counts.values():
+            return 5  # Full house
+        elif 3 in counts.values():
+            return 4  # Three of a kind
+        elif list(counts.values()).count(2) == 2:
+            return 3  # Two pairs
+        elif 2 in counts.values():
+            return 2  # One pair
+    if total_cnt == 5:
+        return 7
+    elif total_cnt == 4:
+        return 6
+
+    if total_cnt == 3 and joker_count == 2:
+        return 4
+    elif total_cnt == 3 and joker_count == 1:
+        cnt_vals.remove(2)
+        if 2 in cnt_vals:
+            return 5
+        return 4
+    elif total_cnt == 2:
+        return 2
+    return 1  # High card
+
+
+def same_rank_comparison(hand1, hand2):
+    for ch1, ch2 in zip(hand1, hand2):
+        if card_types.index(ch1) < card_types.index(ch2):
+            return 1
+        elif card_types.index(ch1) > card_types.index(ch2):
+            return -1
+    return 0
+
+
+def high_card_comparison(hand1, hand2):
+    min_index1 = min([card_types.index(x) for x in hand1])
+    min_index2 = min([card_types.index(x) for x in hand2])
+
+    if min_index1 < min_index2:
+        return 1
+    elif min_index1 > min_index2:
+        return -1
+    return 0
+
+
+def compare_hands(hand1, hand2):
+    # print(hand1, hand2)
+    if get_hand_ranking(hand1) > get_hand_ranking(hand2):
+        return 1
+    elif get_hand_ranking(hand1) < get_hand_ranking(hand2):
+        return - 1
+
+    # if get_hand_ranking(hand1) == 1:
+    #     comp = high_card_comparison(hand1, hand2)
+    #     if comp != 0:
+    #         return comp
+
+    return same_rank_comparison(hand1, hand2)
+
+
+if __name__ == "__main__":
+    hands = []
+
+    for line in read_input():
+        cards, amount = line.split()
+        hands.append((cards, int(amount)))
+    print(hands)
+    hands = sorted(hands, key=cmp_to_key(lambda i1, i2: compare_hands(i1[0], i2[0])))
+    print(hands)
+    sum = 0
+    for i, el in enumerate(hands):
+        sum += el[1] * (i + 1)
+    print(sum)
